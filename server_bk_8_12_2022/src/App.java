b59d409b5d0d3f6802630fb8dc1e36ffd5a0871d
Roy Ruddy Paz Demiquel <ruddypazd@gmail.com>
2022-12-08 00:34:11 -0400

    cambio data
    asdxa

---
+++ server_bk_8_12_2022/src/App.java
@@ -0,0 +1,41 @@
+import Config.Config;
+import SSL.SSL;
+import SocketCliente.SocketCliete;
+import conexion.Conexion;
+import util.console;
+
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        System.setProperty("org.eclipse.jetty.util.log.announce", "false");
+        
+        if (!Config.validate()) {
+            console.error("Server closed.");
+            return;
+        }
+
+        SSL.getKeyStore();
+
+        // Registrar mi certificado default si no existe    
+        if (!SSL.defaultCert()) { 
+            console.error("Server closed.");
+            return;
+        }
+
+        //Cargar el pem del servidor
+        if (!SSL.servicioCert()) {
+            console.error("Server closed.");
+            return;
+        }
+
+        console.log(console.ANSI_GREEN ,SSL.getPem(SSL.getCert(Config.getJSON().getString("nombre"))));
+        Conexion.setConexion(Config.getJSON("data_base"));
+        SocketCliete.Start(Config.getJSON("socket_client").getJSONObject("servicio"));
+
+        
+
+        // SSInit.initFunctions();
+        // ConexionMySql.setConexion(Config.getJSON("data_base_mysql"));
+        //new Migrador().run();
+    }
+}
---
+++ server_bk_8_12_2022/src/component/FondoInversionSucursal.java
@@ -0,0 +1,121 @@
+package component;
+
+import java.io.File;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+import conexion.*;
+import SocketCliente.SocketCliete;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import Config.Config;
+import Server.SSSAbstract.SSServerAbstract;
+import Server.SSSAbstract.SSSessionAbstract;
+
+public class FondoInversionSucursal {
+
+    public static final String nombre_tabla = "fondo_inversion_sucursal";
+
+    public FondoInversionSucursal(JSONObject data, SSSessionAbstract session) {
+        switch (data.getString("type")) {
+            case "getAll":
+                getAll(data, session);
+            break;
+            case "getByKey":
+                getByKey(data, session);
+                break;
+            case "registro":
+                registro(data, session);
+            break;
+            case "editar":
+                editar(data, session);
+            break;
+            case "subirFoto":
+                subirFoto(data, session);
+            break;
+            default:
+                defaultType(data, session);
+        }
+    }
+
+    public void defaultType(JSONObject obj, SSSessionAbstract session) {
+        SocketCliete.send("usuario", obj, session);
+    }
+
+    public void getAll(JSONObject obj, SSSessionAbstract session) {
+        try {
+            String consulta = "select get_all('"+nombre_tabla+"') as json";
+            JSONObject data = Conexion.ejecutarConsultaObject(consulta);
+            Conexion.historico(obj.getString("key_usuario"), nombre_tabla+"_getAll", data);
+            obj.put("data", data);
+            obj.put("estado", "exito");
+        } catch (SQLException e) {
+            obj.put("estado", "error");
+            e.printStackTrace();
+        }
+    }
+    
+
+    public void getByKey(JSONObject obj, SSSessionAbstract session) {
+        try {
+            String consulta =  "select get_by_key('"+nombre_tabla+"','"+obj.getString("key")+"') as json";
+            JSONObject data = Conexion.ejecutarConsultaObject(consulta);
+            Conexion.historico(obj.getString("key_usuario"), nombre_tabla+"_getByKey", data);
+
+            obj.put("data", data);
+            obj.put("estado", "exito");
+        } catch (SQLException e) {
+            obj.put("estado", "error");
+            e.printStackTrace();
+        }
+    }
+
+    public void registro(JSONObject obj, SSSessionAbstract session) {
+        try {
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+            String fecha_on = formatter.format(new Date());
+            JSONObject data = obj.getJSONObject("data");
+            data.put("key",UUID.randomUUID().toString());
+            data.put("fecha_on",fecha_on);
+            data.put("estado",1);
+            Conexion.insertArray(nombre_tabla, new JSONArray().put(data));
+
+            Conexion.historico(obj.getString("key_usuario"), data.getString("key"), nombre_tabla+"_registro", data);
+            obj.put("data", data);
+            obj.put("estado", "exito");            
+            SSServerAbstract.sendServer(SSServerAbstract.TIPO_SOCKET, obj.toString());
+        } catch (SQLException e) {
+            obj.put("estado", "error");
+            e.printStackTrace();
+        }
+
+    }
+
+    public void editar(JSONObject obj, SSSessionAbstract session) {
+        try {
+            JSONObject data = obj.getJSONObject("data");
+            Conexion.editObject(nombre_tabla, data);
+            Conexion.historico(obj.getString("key_usuario"), data.getString("key"), nombre_tabla+"_editar", data);
+            obj.put("data", data);
+            obj.put("estado", "exito");
+            SSServerAbstract.sendServer(SSServerAbstract.TIPO_SOCKET, obj.toString());
+        } catch (SQLException e) {
+            obj.put("estado", "error");
+            obj.put("error", e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void subirFoto(JSONObject obj, SSSessionAbstract session) {
+        String url = Config.getJSON().getJSONObject("files").getString("url");
+        File f = new File(url+nombre_tabla+"/");
+        Conexion.historico(obj.getString("key_usuario"), obj.getString("key"), nombre_tabla+"_subirFoto", new JSONObject());
+        if(!f.exists()) f.mkdirs();
+        obj.put("dirs", new JSONArray().put(f.getPath()+"/"+obj.getString("key")));
+        obj.put("estado", "exito");
+        SSServerAbstract.sendServer(SSServerAbstract.TIPO_SOCKET, obj.toString());
+    }
+}
