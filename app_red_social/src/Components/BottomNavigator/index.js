f0635427a8317327b023e449dd52a94bf544ef9b
Licethd <liceth.dfe@gmail.com>
2023-02-14 00:05:44 -0400

    sucursales

--- app_red_social/src/Components/BottomNavigator/index.js
+++ app_red_social/src/Components/BottomNavigator/index.js
@@ -10,11 +10,22 @@ export default class index extends Component {
     }
 
     getOption = (obj) => {
-        return <SView col={"xs-3"} onPress={() => {
+        return <SView col={"xs-2.4"} onPress={() => {
             SNavigation.navigate(obj.href)
         }} row center height >
             <SView width={8} height />
             <SView width={24} height={24}>
+                {obj.icon}
+            </SView>
+            <SView width={8} height />
+        </SView>
+    }
+    getOption2 = (obj) => {
+        return <SView col={"xs-2.4"} onPress={() => {
+            SNavigation.navigate(obj.href)
+        }} row center height >
+            <SView width={8} height />
+            <SView width={55} height={55}>
                 {obj.icon}
             </SView>
             <SView width={8} height />
@@ -26,6 +37,8 @@ export default class index extends Component {
             <SView col={"xs-11"} row flex center >
                 {this.getOption({ key: "home", icon: <SIcon name={"Home"} fill={STheme.color.primary} stroke={STheme.color.white} />, href: "client" })}
                 {this.getOption({ key: "search", icon: <SIcon name={"Search2"} fill={STheme.color.primary} stroke={STheme.color.white} />, href: "client/search" })}
+                {this.getOption2({ key: "logo", icon: <SIcon name={"LogoBarra"} fill={STheme.color.primary}  style={{position: "absolute", top: -20}}/>, href: "/root" })}
+
                 {this.getOption({ key: "bag", icon: <SIcon name={"Bag"} fill={STheme.color.secondary} stroke={STheme.color.white}  />, href: "" })}
                 {this.getOption({ key: "profile", icon: <SIcon name={"Profile"} fill={STheme.color.primary} stroke={STheme.color.white} />, href: "/login" })}
             </SView>
--- app_red_social/src/Pages/root.js
+++ app_red_social/src/Pages/root.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { SButtom, SHr, SIcon, SImage, SList, SLoad, SNavigation, SPage, SScrollView2, SText, STheme, SView } from 'servisofts-component';
-import { BottomNavigator, Container, NavBar, Pedido, Restaurante, TopBar } from '../Components';
+import { BottomNavigator, Container, NavBar, Pedido, Restaurante, TopBar, Sucursal } from '../Components';
 import Model from '../Model';
 import SSocket from 'servisofts-socket'
 class index extends Component {
@@ -21,112 +21,134 @@ class index extends Component {
         Model.favorito.Action.CLEAR();
         Model.publicacion.Action.CLEAR();
     }
-    loadData() {
-        this.restaurantes = Model.restaurante.Action.getAllFilters();
-        this.pedidos_en_curso = Model.pedido.Action.getEnCurso();
-        if (!this.restaurantes) return null;
-        // if (!this.pedidos_en_curso) return null;
-        return true;
-    }
+    // loadData() {
+    //     this.restaurantes = Model.restaurante.Action.getAllFilters();
+    //     this.pedidos_en_curso = Model.pedido.Action.getEnCurso();
+    //     if (!this.restaurantes) return null;
+    //     // if (!this.pedidos_en_curso) return null;
+    //     return true;
+    // }
 
-    render_list_recomendados() {
-        if (!this.loadData()) return <SLoad />
-        return <SView col={"xs-12"} height={195}>
-            <SScrollView2>
-                <SList horizontal center initSpace={8}
-                    data={this.restaurantes}
-                    limit={5}
-                    render={(obj) => {
-                        return <Restaurante.Card data={obj} onPress={(data) => {
-                            SNavigation.navigate("/restaurante", { pk: data.key })
-                        }} />
-                    }}
-                />
-            </SScrollView2>
-        </SView>
-    }
-    render_list_cerca() {
-        if (!this.loadData()) return <SLoad />
-        return <SView col={"xs-12"} height={195}>
-            <SScrollView2>
-                <SList horizontal center initSpace={8}
-                    data={this.restaurantes}
-                    limit={5}
-                    order={[{ key: "distancia", order: "asc", peso: 1 }]}
-                    render={(obj) => {
-                        return <Restaurante.Card data={obj} onPress={(data) => {
-                            SNavigation.navigate("/restaurante", { pk: data.key })
-                        }} />
-                    }}
-                />
-            </SScrollView2>
-        </SView>
-    }
-    render_pedidos_en_curso() {
-        if (!this.pedidos_en_curso) return null;
-        if (!this.pedidos_en_curso.length) return null;
-        return <SView col={"xs-12"} height={195}>
-            <SHr height={20} />
-            <SText style={{ paddingLeft: 4, fontSize: 18 }} bold>{"Pedidos en curso"}</SText>
-            <SHr />
-            <SScrollView2>
-                <SList horizontal center initSpace={8}
-                    data={this.pedidos_en_curso}
-                    limit={5}
-                    render={(obj) => {
-                        return <Pedido.Card data={obj} onPress={(data) => {
-                            SNavigation.navigate("/pedido", { pk: data.key })
-                        }} />
-                    }}
-                />
-            </SScrollView2>
-        </SView>
-    }
-    render_novedades() {
-        var novedades = Model.novedades.Action.getAll();
-        if (!novedades) return null
-        return <SView col={"xs-12"} height={195}>
-            <SScrollView2>
-                <SList
-                    horizontal
-                    initSpace={8}
-                    data={novedades}
-                    render={(data) => {
-                        return <SView card width={318} height={150} style={{
-                            overflow: "hidden"
-                        }}>
-                            <SImage src={SSocket.api.root + "novedades/" + data.key} style={{
-                                resizeMode: "cover"
-                            }} />
-                        </SView>
-                    }}
-                />
-            </SScrollView2>
-        </SView>
-    }
+    // render_list_recomendados() {
+    //     if (!this.loadData()) return <SLoad />
+    //     return <SView col={"xs-12"} height={195}>
+    //         <SScrollView2>
+    //             <SList horizontal center initSpace={8}
+    //                 data={this.restaurantes}
+    //                 limit={5}
+    //                 render={(obj) => {
+    //                     return <Restaurante.Card data={obj} onPress={(data) => {
+    //                         SNavigation.navigate("/restaurante", { pk: data.key })
+    //                     }} />
+    //                 }}
+    //             />
+    //         </SScrollView2>
+    //     </SView>
+    // }
+    // render_list_cerca() {
+    //     if (!this.loadData()) return <SLoad />
+    //     return <SView col={"xs-12"} height={195}>
+    //         <SScrollView2>
+    //             <SList horizontal center initSpace={8}
+    //                 data={this.restaurantes}
+    //                 limit={5}
+    //                 order={[{ key: "distancia", order: "asc", peso: 1 }]}
+    //                 render={(obj) => {
+    //                     return <Restaurante.Card data={obj} onPress={(data) => {
+    //                         SNavigation.navigate("/restaurante", { pk: data.key })
+    //                     }} />
+    //                 }}
+    //             />
+    //         </SScrollView2>
+    //     </SView>
+    // }
+    // render_pedidos_en_curso() {
+    //     if (!this.pedidos_en_curso) return null;
+    //     if (!this.pedidos_en_curso.length) return null;
+    //     return <SView col={"xs-12"} height={195}>
+    //         <SHr height={20} />
+    //         <SText style={{ paddingLeft: 4, fontSize: 18 }} bold>{"Pedidos en curso"}</SText>
+    //         <SHr />
+    //         <SScrollView2>
+    //             <SList horizontal center initSpace={8}
+    //                 data={this.pedidos_en_curso}
+    //                 limit={5}
+    //                 render={(obj) => {
+    //                     return <Pedido.Card data={obj} onPress={(data) => {
+    //                         SNavigation.navigate("/pedido", { pk: data.key })
+    //                     }} />
+    //                 }}
+    //             />
+    //         </SScrollView2>
+    //     </SView>
+    // }
+
+    // render_novedades() {
+    //     var novedades = Model.novedades.Action.getAll();
+    //     if (!novedades) return null
+    //     return <SView col={"xs-12"} height={195}>
+    //         <SScrollView2>
+    //             <SList
+    //                 horizontal
+    //                 initSpace={8}
+    //                 data={novedades}
+    //                 render={(data) => {
+    //                     return <SView card width={318} height={150} style={{
+    //                         overflow: "hidden"
+    //                     }}>
+    //                         <SImage src={SSocket.api.root + "novedades/" + data.key} style={{
+    //                             resizeMode: "cover"
+    //                         }} />
+    //                     </SView>
+    //                 }}
+    //             />
+    //         </SScrollView2>
+    //     </SView>
+    // }
     render_with_data() {
-        var sucursales = Model.sucursales.Action.getAll();
-        // var usuarios = Usuario.Actions.getAll(this.props);
+        var sucursales = Model.filtros.Action.getAll();
+        //var usuarios = Model.filtros.Action.getAll();
         if (!sucursales) return <SLoad />
         // if (!usuarios) usuarios = {}
 
+        console.log(JSON.stringify(sucursales) + " aquiiii")
+        var j = 1;
+
         return <SList
+            buscador={"true"}
             center
-            space={32}
+            space={5}
             data={Object.values(sucursales)}
-            order={[{ key: "fecha_on", order: "desc", peso: 1, }]}
+            // order={[{ key: "fecha_on", order: "desc", peso: 1, }]}
             render={(data) => {
-                return <SView card width={318} height={150} style={{
-                    overflow: "hidden"
-                }}>
-                    <SText>AA</SText>
-                    {/* <SImage src={SSocket.api.root + "novedades/" + data.key} style={{
-                        resizeMode: "cover"
-                    }} /> */}
-                    {/* <SImage src={SSocket.api.root + "novedades/" + data.key} style={{
-                        resizeMode: "cover"
-                    }} /> */}
-                </SView>
+                return <Sucursal.Card image={2} nombre = {"CENTRO"}></Sucursal.Card>
+                // <SView col={"xs-12"} row>
+                //     <SView col={"xs-12"} row height={130} backgroundColor={STheme.color.card} style={{ borderRadius: 24, }} onPress={this.props.onPress}>
+                //         <SView col={"xs-12"} center height >
+                //             <SView col={"xs-12"} height={130} style={{
+                //                 borderRadius: 8,
+                //                 backgroundColor: STheme.color.card,
+                //                 overflow: 'hidden',
+                //             }}>
+                //                 <SImage enablePreview src={require('../Assets/img/s' + j + ".jpg")} width={"100%"} height={"100%"}
+                //                     style={{
+                //                         resizeMode: 'cover',
+                //                     }}
+                //                 />
+                //             </SView>
+                //         </SView>
+                //     </SView>
+                //     <SView col={"xs-12"} flex style={{ alignItems: "flex-end", bottom: -85, right: 10 }} >
+                //         <SView>
+                //             <SText fontSize={15} color={STheme.color.white}>CENTRO</SText>
+                //         </SView>
+                //         <SHr />
+                //         <SIcon name={"Vineta1"} height={12} width={6} stroke={STheme.color.white}></SIcon>
+                //     </SView>
+                //     <SView col={"xs-12"} row >
+                //         <SText style={{ alignItems: "flex-end", top: -20, right: 25 }} flex fontSize={10} color={STheme.color.white}>Av. La barranca 3er anillo frente al surtidor Genex</SText>
+                //     </SView>
+                // </SView>
             }}
         />
 
@@ -149,12 +171,7 @@ class index extends Component {
                 onRefresh={this.clearData}
             >
                 <Container>
-                    {/* {this.render_with_data()} */}
-                    <SView col={"xs-12"} row height={105} style={{borderRadius:20}}>
-                        <SImage src={require('../Assets/img/s1.jpg')} style={{ height: 106,borderRadius:20,overflow:"hidden" }} />
-                    </SView>
-
-
+                    {this.render_with_data()}
                 </Container>
             </SPage>
         );
