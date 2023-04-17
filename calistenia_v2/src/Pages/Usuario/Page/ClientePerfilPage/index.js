9126fb19fd54af44fd3f29bd85b96ada4980c883
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2023-04-16 20:47:07 -0400

    Update index.js

--- calistenia_v2/src/Pages/Usuario/Page/ClientePerfilPage/index.js
+++ calistenia_v2/src/Pages/Usuario/Page/ClientePerfilPage/index.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import { ScrollView, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { SButtom, SHr, SLoad, SNavigation, SPage, STheme, SView } from 'servisofts-component';
+import { SButtom, SHr, SLoad, SNavigation, SOrdenador, SPage, STheme, SView } from 'servisofts-component';
 // import BackgroundImage from '../../Component/BackgroundImage';
 import BarraSuperior from '../../../../Components/BarraSuperior';
 import FotoPerfilUsuario from '../../../../Components/FotoPerfilUsuario';
@@ -11,6 +11,11 @@ import SincronizarUsuario from '../../../../Services/zkteco/Components/usuario_h
 import PaquetesDeUsuario from './PaquetesDeUsuario';
 // import PaquetesDeUsuario from './PaquetesDeUsuario';
 // import SSCrollView from '../../Component/SScrollView';
+import SSocket from 'servisofts-socket';
+
+import Paquete from '../../../Paquete';
+import Sucursal from '../../../Sucursal';
+import sucursal_usuario from '../../../sucursal_usuario';
 class ClientePerfilPage extends Component {
   static navigationOptions = ({ navigation }) => {
     return { headerShown: false, }
@@ -49,6 +54,8 @@ class ClientePerfilPage extends Component {
     if (correo.length < 12) this.bandera = true;
     return <Text style={{ fontSize: 16, color: (correo.length < 12 ? "red" : STheme.color.text), }}>{"Correo: " + correo}</Text>
   }
+
+
   getPerfil() {
     this.data = Model.usuario.Action.getByKey(this.key);
     if (!this.data) return <SLoad />
@@ -95,6 +102,49 @@ class ClientePerfilPage extends Component {
       <SHr />
     </SView >
   }
+
+  exportar(_key) {
+    var reducer = this.props.state.paqueteVentaReducer;
+    var data = reducer.usuario[_key];
+    if (!data) {
+      if (reducer.estado == "cargando") return null;
+      if (reducer.estado == "error") return <Text>ERROR</Text>
+      var object = {
+        component: "paqueteVenta",
+        type: "getAllByUsuario",
+        estado: "cargando",
+        key_usuario: _key
+      }
+      SSocket.send(object, true);
+      // return <View />
+    }
+    return new SOrdenador([
+      { key: "fecha_inicio", order: "desc", peso: 1 },
+    ]).ordernarObject(data).map((key) => {
+
+      var obj = data[key];
+
+      if (!sucursal_usuario.Actions.isActive(obj.key_sucursal, this.props)) return null;
+
+      var usuarios = Model.usuario.Action.getAll();
+      if (!usuarios) return <SLoad />
+      // var usuario = Object.values(usuarios).find(aux => aux.key == obj.key_usuario);
+      var usuario = usuarios[obj.key_usuario];
+
+      var paquetes = Paquete.Actions.getAll(this.props);
+      if (!paquetes) return null;
+      // var paquete = Object.values(paquetes).find(aux => aux.key == obj.key_paquete);
+      var paquete = paquetes[obj.key_paquete];
+
+      var sucursales = Sucursal.Actions.getAll(this.props);
+      if (!sucursales) return null;
+      // var sucursal = Object.values(sucursales).find(aux => aux.key == obj.key_sucursal);
+      var sucursal = sucursales[obj.key_sucursal];
+
+      console.log("aqui ", obj, " pack ", paquete.descripcion, " sucursal ", sucursal.descripcion, " usuario ", usuario.Nombres);
+    })
+    // return <Text style={{ fontSize: 16, color: STheme.color.text }}>Correo </Text>
+  }
   render() {
     return (
       <SPage hidden >
@@ -104,6 +154,7 @@ class ClientePerfilPage extends Component {
         <ScrollView>
           <SView col={"xs-12"} center>
             {this.getPerfil()}
+            {this.exportar(this.key)}
             <SView col={"xs-11 md-8 xl-6"} center>
               <PaquetesDeUsuario key_usuario={this.key} navigation={this.props.navigation} />
             </SView>
